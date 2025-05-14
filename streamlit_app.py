f9df2ef649c208a2e9e1a7ff0fdfab8dc95b35a9
SouMahdi <soumayaamahdi@gmail.com>
2025-05-14 11:22:49 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 
 # ---------------------------- CONFIG ----------------------------
 MODEL_FILENAME = "unet_mobilenetv2_finetuned_model.h5"
-DRIVE_FILE_ID = "1ABCdEfGhIjKlMnOP"  # ⬅️ Replace with your actual file ID
+DRIVE_FILE_ID = "1jz5EMrf-DILqHg33MWXstPHewvrMCvgu"  
 INPUT_SIZE = (224, 224)
 THRESHOLD = 0.5  # For converting probabilities to binary mask
 
